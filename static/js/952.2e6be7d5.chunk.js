a7919b3bf8031cf5b171ec35842db4b740e4c775
Belal Sajal <h_belal@outlook.com>
2025-04-15 13:05:18 +0200

    Updates

---
+++ static/js/952.2e6be7d5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkglobal_pickups=self.webpackChunkglobal_pickups||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.2e6be7d5.chunk.js.map
